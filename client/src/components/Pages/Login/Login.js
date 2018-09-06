73c1d5df2e8076dd4f3a41ad5c727c037447e7a2
Steve <bosilovatz@gmail.com>
2018-09-05 19:17:25 -0500

    Update search function, start adding login and registration

--- client/src/components/Pages/Login/Login.js
+++ client/src/components/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, ControlLabel, Col, FormControl, Button } from 'react-bootstrap';
 import Toolbar from '../../Navigation/Toolbar';
 import './Login.css'
 
@@ -7,7 +8,37 @@ export default class Login extends Component {
     return (
       <div className="Login">
         <Toolbar />
-        <h1> Login Page </h1>
+        <Form horizontal>
+          <FormGroup controlId="email">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl
+                autoFocus
+                type="email"
+                placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl
+                type="password"
+                placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+        <p> No account? Click <a href="/registration" target=""> here </a> to create an account. </p>
       </div>
     )
   }
